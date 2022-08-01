d5776f7b9d0eacb621c702f6a293eec3037cdb76
Luciano Muniz <luciano.mo@gmail.com>
2022-08-01 20:22:00 -0300

    added songDetail and lyricCreate and lyricList

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { identity } from 'lodash';
+
+class LyricCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: '',
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        content: this.state.content,
+        songId: this.props.songId,
+      },
+    });
+    this.setState({ content: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <label>Add a Lyric</label>
+        <input
+          value={this.state.content}
+          onChange={(e) => this.setState({ content: e.target.value })}
+        />
+      </form>
+    );
+  }
+}
+
+const mutation = gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        id
+        content
+      }
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricCreate);
